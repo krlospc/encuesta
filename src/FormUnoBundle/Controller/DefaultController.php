72f5f1a51c530610bd6165c2bcf9ead4798bcae6
Charly Jimenez <charlyjm@gmail.com>
2018-10-15 15:55:50 -0400

    Priemra validacion

--- src/FormUnoBundle/Controller/DefaultController.php
+++ src/FormUnoBundle/Controller/DefaultController.php
@@ -25,94 +25,110 @@ class DefaultController extends Controller
         $em = $this->getDoctrine()->getManager();        
         $db = $em->getConnection();
 
-        $factor_tipo_1 = "Dentro de la unidad educativa: factores atribuidos al estudiante";
-        $factor_tipo_2 = "Dentro de la unidad educativa: factores atribuidos a la unidad educativa";
-        $factor_tipo_3 = "Fuera de la unidad educativa: factores familiares";
-        $factor_tipo_4 = "Fuera de la unidad educativa: factores economicos";
-        $factor_tipo_5 = "Fuera de la unidad educativa: factores sociales";
-        $factor_tipo_6 = "Otros factores";
-
-        $array = [
-            "1" => "Enfermedad",
-            "2" => "Accidente",
-            "3" => "Discapacidad",
-            "4" => "Sobre edad",
-            "5" => "Precocidad",
-            "6" => "Embarazo/Paternidad",
-            "7" => "El estudiante ya formo su propio hogar",
-            "8" => "Abandono en la gestion 2016",
-            "9" => "Reprobación en la gestion 2016",
-            "10" => "Se inscribió a un Centro de Educación Alternativa (CEA)",
-            "11" => "Bullying",
-            "12" => "La Unidad educativa es distante",
-            "13" => "La Unidad Educativa no oferta el turno requerido por algunos estudiantes",
-            "14" => "Responsabilidades en el hogar:QUehaceres domésticos (cocinar, lavar, planchar),atención a niños, a ancianos, a personas enfermas o con discapacidad",
-            "15" => "Traslado de la familia a otros: Departamentos,Provincias,Municipios,Barrios,Países",
-            "16" => "Falta de apoyo de los familiares para la continuación de sus estudios",
-            "17" => "No vive en un hogar nuclear completo (Su Padre y/o su Madre no viven con él)",
-            "18" => "Separación de ls miembros del hogar (Padres, hermanos,parientes cercanos,etc)",
-            "19" => "Violencia familiar",
-            "20" => "Fallecimiento o accidente de algún familiar",
-            "21" => "Falta de dinero",
-            "22" => "Trabajo de los estudiantes",
-            "23" => "Por trabajo de los padres",
-            "24" => "Pandillas",
-            "25" => "Drogas",
-            "26" => "Alcoholismo",
-            "27" => "Otra (especifique)",
-            "28" => "Otra (especifique)"            
-        ];
-
-        $query = "INSERT INTO formulario_uno_index (dep, cod_unidad_educativa, nboleta, nombre, paterno, materno, unidad_educativa, sexo, telefono, cargo, p8, p9, p10, p12, p13) ";
-        $query = $query."VALUES ('', '', '', '', '', '', '', '', '', '', '', '', '', '', '')";          
-        $stmt = $db->prepare($query);
-        $params = array();
-        $stmt->execute($params);
-
-        $id = $db->lastInsertId();
-        
-        for ($i = 1; $i <= 28; $i++) {
-
-            $factor_tipo = "";
-            if (($i >= 1) && ($i <= 10)){
-                $factor_tipo = $factor_tipo_1;
-            }
-            if (($i >= 11) && ($i <= 13)){
-                $factor_tipo = $factor_tipo_2;
-            }
-            if (($i >= 14) && ($i <= 20)){
-                $factor_tipo = $factor_tipo_3;
-            }
-            if (($i >= 21) && ($i <= 23)){
-                $factor_tipo = $factor_tipo_4;
-            }
-            if (($i >= 24) && ($i <= 26)){
-                $factor_tipo = $factor_tipo_5;
-            }
-            if (($i >= 27) && ($i <= 28)){
-                $factor_tipo = $factor_tipo_6;
-            }
-
-            $query = "INSERT INTO formulario_uno_factores (factor_tipo, factor, seleccion, orden_importancia, por_que, cod, formulario_uno_index_id) ";
-            $query = $query."VALUES ('".$factor_tipo."', '".$array[$i]."', false, 0, '' ,'".$i."','".$id."')";            
-            $stmt = $db->prepare($query);
-            $params = array();
-            $stmt->execute($params);            
-        }
-
-        $query = "select * from  formulario_uno_index where id = ".$id;
-        $stmt = $db->prepare($query);
-        $params = array();
-        $stmt->execute($params);
-        $po_index = $stmt->fetchAll();
-
-        $query = "select * from  formulario_uno_factores where formulario_uno_index_id = ".$id;
+        $query = 'SELECT * FROM formulario_uno_index a WHERE a.nombre = "" AND a.paterno = "" AND a.materno = ""';
         $stmt = $db->prepare($query);
         $params = array();
         $stmt->execute($params);
         $po_fac = $stmt->fetchAll();
 
-        return $this->render('FormUnoBundle:Default:new.html.twig', array('po_index' => $po_index, 'po_fac' => $po_fac,));
+        //dump(count($po_fac));die;
+        if (count($po_fac) > 0){
+            $query = "select * from  formulario_uno_index";
+            $stmt = $db->prepare($query);
+            $params = array();
+            $stmt->execute($params);
+            $po = $stmt->fetchAll();                
+            return $this->render('FormUnoBundle:Default:index.html.twig', array('po' => $po));
+        } else {
+            $factor_tipo_1 = "Dentro de la unidad educativa: factores atribuidos al estudiante";
+            $factor_tipo_2 = "Dentro de la unidad educativa: factores atribuidos a la unidad educativa";
+            $factor_tipo_3 = "Fuera de la unidad educativa: factores familiares";
+            $factor_tipo_4 = "Fuera de la unidad educativa: factores economicos";
+            $factor_tipo_5 = "Fuera de la unidad educativa: factores sociales";
+            $factor_tipo_6 = "Otros factores";
+
+            $array = [
+                "1" => "Enfermedad",
+                "2" => "Accidente",
+                "3" => "Discapacidad",
+                "4" => "Sobre edad",
+                "5" => "Precocidad",
+                "6" => "Embarazo/Paternidad",
+                "7" => "El estudiante ya formo su propio hogar",
+                "8" => "Abandono en la gestion 2016",
+                "9" => "Reprobación en la gestion 2016",
+                "10" => "Se inscribió a un Centro de Educación Alternativa (CEA)",
+                "11" => "Bullying",
+                "12" => "La Unidad educativa es distante",
+                "13" => "La Unidad Educativa no oferta el turno requerido por algunos estudiantes",
+                "14" => "Responsabilidades en el hogar:QUehaceres domésticos (cocinar, lavar, planchar),atención a niños, a ancianos, a personas enfermas o con discapacidad",
+                "15" => "Traslado de la familia a otros: Departamentos,Provincias,Municipios,Barrios,Países",
+                "16" => "Falta de apoyo de los familiares para la continuación de sus estudios",
+                "17" => "No vive en un hogar nuclear completo (Su Padre y/o su Madre no viven con él)",
+                "18" => "Separación de ls miembros del hogar (Padres, hermanos,parientes cercanos,etc)",
+                "19" => "Violencia familiar",
+                "20" => "Fallecimiento o accidente de algún familiar",
+                "21" => "Falta de dinero",
+                "22" => "Trabajo de los estudiantes",
+                "23" => "Por trabajo de los padres",
+                "24" => "Pandillas",
+                "25" => "Drogas",
+                "26" => "Alcoholismo",
+                "27" => "Otra (especifique)",
+                "28" => "Otra (especifique)"            
+            ];
+
+            $query = "INSERT INTO formulario_uno_index (dep, cod_unidad_educativa, nboleta, nombre, paterno, materno, unidad_educativa, sexo, telefono, cargo, p8, p9, p10, p12, p13) ";
+            $query = $query."VALUES ('', '', '', '', '', '', '', '', '', '', '', '', '', '', '')";          
+            $stmt = $db->prepare($query);
+            $params = array();
+            $stmt->execute($params);
+
+            $id = $db->lastInsertId();
+            
+            for ($i = 1; $i <= 28; $i++) {
+
+                $factor_tipo = "";
+                if (($i >= 1) && ($i <= 10)){
+                    $factor_tipo = $factor_tipo_1;
+                }
+                if (($i >= 11) && ($i <= 13)){
+                    $factor_tipo = $factor_tipo_2;
+                }
+                if (($i >= 14) && ($i <= 20)){
+                    $factor_tipo = $factor_tipo_3;
+                }
+                if (($i >= 21) && ($i <= 23)){
+                    $factor_tipo = $factor_tipo_4;
+                }
+                if (($i >= 24) && ($i <= 26)){
+                    $factor_tipo = $factor_tipo_5;
+                }
+                if (($i >= 27) && ($i <= 28)){
+                    $factor_tipo = $factor_tipo_6;
+                }
+
+                $query = "INSERT INTO formulario_uno_factores (factor_tipo, factor, seleccion, orden_importancia, por_que, cod, formulario_uno_index_id) ";
+                $query = $query."VALUES ('".$factor_tipo."', '".$array[$i]."', false, 0, '' ,'".$i."','".$id."')";            
+                $stmt = $db->prepare($query);
+                $params = array();
+                $stmt->execute($params);            
+            }
+
+            $query = "select * from  formulario_uno_index where id = ".$id;
+            $stmt = $db->prepare($query);
+            $params = array();
+            $stmt->execute($params);
+            $po_index = $stmt->fetchAll();
+
+            $query = "select * from  formulario_uno_factores where formulario_uno_index_id = ".$id;
+            $stmt = $db->prepare($query);
+            $params = array();
+            $stmt->execute($params);
+            $po_fac = $stmt->fetchAll();
+
+            return $this->render('FormUnoBundle:Default:new.html.twig', array('po_index' => $po_index, 'po_fac' => $po_fac,));
+        }    
     }
 
     public function insertAction(Request $request)
@@ -195,14 +211,9 @@ class DefaultController extends Controller
     public function deleteAction($idfactor)
     {
         $em = $this->getDoctrine()->getManager();        
-        $db = $em->getConnection();
+        $db = $em->getConnection();        
 
-        //dump($idfactor);die();
-        
         $query = "DELETE FROM formulario_uno_factores WHERE formulario_uno_index_id = ".$idfactor;
-
-        
-        //dump($query);die();
         $stmt = $db->prepare($query);
         $params = array();
         $stmt->execute($params); 
